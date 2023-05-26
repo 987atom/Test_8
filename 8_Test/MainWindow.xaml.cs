1cbf9a3412e09b393f9e418c1db8a766be70ac7d
987atom <isip_m.k.davydov@mpt.ru>
2023-05-26 22:01:13 +0300

    first

---
+++ 8_Test/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Library;
+
+namespace _8_Test
+{
+    public partial class MainWindow : Window
+    {
+        int count = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public class MyClass
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        private void serial_Click(object sender, RoutedEventArgs e)
+        {
+            string txt = txt_ser.Text;
+
+            MyClass obj1 = new MyClass { Id = count, Name = txt };
+            count++;
+            DataSerialization.SerializeToFile(obj1, path.Text);
+        }
+
+        private void deser_Click(object sender, RoutedEventArgs e)
+        {
+            MyClass result1 = DataSerialization.DeserializeFromFile<MyClass>(path.Text);
+
+            txt_deser.Text = result1.Name;
+        }
+    }
+}
---
+++ Library/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace Library
+{
+    public class DataSerialization
+    {
+        // Сериализация данных объекта в JSON и запись в файл
+        public static void SerializeToFile<T>(T data, string filePath)
+        {
+            string jsonData = JsonConvert.SerializeObject(data);
+            File.WriteAllText(filePath, jsonData);
+        }
+
+        // Десериализация данных из файла в объект заданного типа
+        public static T DeserializeFromFile<T>(string filePath)
+        {
+            string jsonData = File.ReadAllText(filePath);
+            T data = JsonConvert.DeserializeObject<T>(jsonData);
+            return data;
+        }
+    }
+}
